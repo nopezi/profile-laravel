569f1d0d22054f6798ebd0e3b2a506dc97b4a32b
nopezi saputra <snopezi@gmail.com>
2020-11-22 20:38:33 +0700

    url secure

--- resources/views/home/gallery.blade.php
+++ resources/views/home/gallery.blade.php
@@ -7,18 +7,18 @@
     </div>
 
     <div class="owl-carousel gallery-carousel" data-aos="fade-up">
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-1.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-1.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-2.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-2.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-3.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-3.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-4.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-4.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-5.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-5.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-6.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-6.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-7.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-7.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-8.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-8.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-9.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-9.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-10.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-10.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-11.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-11.png') }}" alt=""></a>
-      <a href="{{ asset('Appland/assets/img/gallery/gallery-12.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ asset('Appland/assets/img/gallery/gallery-12.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-1.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-1.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-2.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-2.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-3.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-3.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-4.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-4.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-5.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-5.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-6.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-6.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-7.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-7.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-8.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-8.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-9.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-9.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-10.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-10.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-11.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-11.png') }}" alt=""></a>
+      <a href="{{ secure_asset('Appland/assets/img/gallery/gallery-12.png') }}" class="venobox" data-gall="gallery-carousel"><img src="{{ secure_asset('Appland/assets/img/gallery/gallery-12.png') }}" alt=""></a>
     </div>
 
   </div>
