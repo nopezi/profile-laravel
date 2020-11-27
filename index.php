467c1f029d9d0eee199b275af74ddfbdcf7bdb45
nopezi saputra <snopezi@gmail.com>
2020-11-27 15:23:49 +0700

    composer

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$protocol='http';
+if (isset($_SERVER['HTTPS']))
+  if (strtoupper($_SERVER['HTTPS'])=='ON')
+    $protocol='https';
+
+header("location: $protocol://".$_SERVER['HTTP_HOST']."/?para=abc");
+
+// header('Location: public/');
